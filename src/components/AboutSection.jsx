19ea756ca5b02292d6ce6ecea2a60b47d7a11736
santhosh-mrav <santhoshmodi051@gmail.com>
2024-08-07 14:35:35 +0530

    modificatio done

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ const AboutSection = () => {
                             </div>
                         </div>
                         <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-black sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                            <p className="leading-relaxed text-lg mb-4 sm:text-justify">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
+                            <p className="leading-relaxed text-lg mb-4 sm:text-justify"> book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
                             
                             <div className='btns'>
                                 <button onClick={handleResumeClick} className='bg-orange-500 mr-4 w-24 rounded '> <FontAwesomeIcon icon={faFile} className='text-black' />Resume

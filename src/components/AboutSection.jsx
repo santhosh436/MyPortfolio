b4028692a1ba71786c8805522c96fbd6a96dede9
santhosh-mrav <santhoshmodi051@gmail.com>
2024-08-07 14:32:00 +0530

    adding the files in first

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+const AboutSection = () => {
+    const handleResumeClick = () => {
+        // Logic for resume button click
+        console.log('Resume button clicked');
+      };
+
+    const handlePortfolioClick = () => {
+    // Logic for portfolio button click
+    console.log('Portfolio button clicked');
+    };
+    
+  return (
+    <div>
+        <section className="text-gray-600 body-font" id='about'>
+            <div className="container px-5 py-24 mx-auto bg-white flex flex-col relative rounded-lg mb-11">
+                    <p className='absolute top-9 left-32 sm:left-20 text-2xl font-bold  text-center sm:text-left'>Who am I ?</p>
+                <div className="lg:5/12">
+                    <div className="flex flex-col sm:flex-row mt-4">
+                        <div className="sm:w-1/3 text-center sm:pr-3 sm:py-6 bg-orange-500 mr-5 rounded-3xl">
+                            <div className="w-24 h-24 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                                 <img src="./images/ProfileJpg.png" className='rounded-full object-contain' alt="" />
+                            </div>
+                            <div className="flex flex-col items-center text-center justify-center">
+                                <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-2"></div>
+                                <h2 className="font-bold title-font text-lg text-white">Junior Full Stack Developer</h2>
+                                <h3 className='text-gray-700 font-medium'>Front End Developer | Back End Developer</h3>
+                                <p className="text-base w-96 text-center"></p>
+                            </div>
+                        </div>
+                        <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-black sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                            <p className="leading-relaxed text-lg mb-4 sm:text-justify">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
+                            
+                            <div className='btns'>
+                                <button onClick={handleResumeClick} className='bg-orange-500 mr-4 w-24 rounded '> <FontAwesomeIcon icon={faFile} className='text-black' />Resume
+                                </button>
+                                <button onClick={handlePortfolioClick} className='bg-orange-500 w-24 rounded' >Portfolio <span>&#8594;</span>
+                                </button>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    </div>
+  )
+}
+
+export default AboutSection
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,33 @@
+// App.js
+import React from 'react';
+import '../App.css'; // Ensure you have Tailwind CSS imported in your CSS file
+
+const MainSection = () => {
+  return (
+    <section className="flex flex-col items-center md:flex-row text-white h-screen sm:pl-32 mb-40 sm:mb-0" id='home'>
+      {/* Portfolio Column */}
+      <div className="flex-1 p-4 sm:text-left mt-4">
+        <p className=' sm:text-left text-xl mt-16'>Hello!</p>
+        <h1 className="sm:text-5xl text-2xl font-bold mb-1">I'm Santhosh Modi <br /> <span className='text-orange-400'>Junior Full Stack Developer</span></h1>
+        <p className='sm:text-xl text-md text-left mb-3 font-semibold text-orange-500'>FrontEnd Developer | BackEnd Developer</p>
+        <p className="text-lg mb-4">
+        Welcome to my professional portfolio! As a full-stack developer, I strive to
+        create fast, responsive and user-friendly web applications. Explore my
+        projects, skills and contact me for partnerships and tech opportunities.
+        </p>
+        <button className='bg-orange-500 w-40 h-10 rounded text-xl'><a href="#about">About Me <span className='text-2xl'>&#x2192;</span></a></button>
+      </div>
+
+      {/* Image Column */}
+      <div className="flex-1 p-4 flex justify-center items-center ">
+        <img
+          src="./images/ProfileJpg.png"
+          alt="My Portfolio"
+          className="lg:w-9/12 w-full lg:h-auto h-64 object-cover object-center rounded"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default MainSection;

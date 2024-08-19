f768fe4697a65e0f85666c8d8e4753e3c2ed4658
santhosh-mrav <santhoshmodi051@gmail.com>
2024-08-19 11:45:03 +0530

    commited

--- src/App1.js
+++ src/App1.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.css';
-import {BrowserRouter as Rounter,Route, Routes } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage'
 import PortfolioPage from './Pages/PortfolioPage'
 import ContactPage from './Pages/ContactPage'
@@ -8,14 +8,11 @@ import ContactPage from './Pages/ContactPage'
 const App = () => {
   return (
     <div>
-      <Rounter>
         <Routes>
           <Route path='/' element= { <LandingPage/> } />
           <Route path='/portfolio' element= { <PortfolioPage/> } />
           <Route path='/contact' element= { <ContactPage/> } />
         </Routes>
-      </Rounter>
-
     </div>
   )
 }
